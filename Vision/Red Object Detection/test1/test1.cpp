2a57665cb3cde4696d92a5229aace56e8f4cdb5d
sujoe77 <sujoe77@gmail.com>
2019-08-03 11:45:11 +0200

    OpenCV make it work on x64

--- Vision/Red Object Detection/test1/test1.cpp
+++ Vision/Red Object Detection/test1/test1.cpp
@@ -17,7 +17,7 @@ using namespace std;
          return -1;
     }
 
-    namedWindow("Control",CV_WINDOW_AUTOSIZE); //create a window called "Control"
+    namedWindow("Control",WINDOW_AUTOSIZE); //create a window called "Control"
 
 	int iLowH = 0;
 	int iHighH = 179;
@@ -29,14 +29,14 @@ using namespace std;
 	int iHighV = 255;
 
 	//Create trackbars in "Control" window
-	cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-	cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	createTrackbar("HighH", "Control", &iHighH, 179);
 
-	cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-	cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	createTrackbar("HighS", "Control", &iHighS, 255);
 
-	cvCreateTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
-	cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+	createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+	createTrackbar("HighV", "Control", &iHighV, 255);
 
     while (true)
     {
