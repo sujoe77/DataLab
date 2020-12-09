9cbf21cb332d6d729327defd92adc139b52b4d97
sujoe <sujoe77@gmail.com>
2020-12-09 04:52:15 +0100

    update ItJobBank.py and StackOverflow.py

--- cs109/webScraping/job/ItJobBank.py
+++ cs109/webScraping/job/ItJobBank.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import datetime
+from datetime import datetime
 from job import Job
 
 class ItJobBankJob(Job):
@@ -13,22 +14,29 @@ class ItJobBankJob(Job):
 
     def parse(self, page_content):
         result = []
-        h3_title = page_content.find_all("h3", "title")
-        div_info = page_content.find_all("div", "info")
-        div_time= page_content.find_all("time")        
+        h3_title = page_content.find_all("h3", "job-title")
+        div_info = page_content.find_all("div", "job-info")
+        div_company = page_content.find_all("div", "job-company")
+        div_time= page_content.find_all("time")
+        #span_location = page_content.find_all("span", "job-location")
         divSize = len(h3_title)
         for i in range(0, divSize):
-            print(h3_title[i].a["href"])
-            print(h3_title[i].text)
-            print(div_info[i].text)
-            print(div_time[i]["datetime"])
-            print()
+            #print(div_company[i].text)
+            #print(h3_title[i].a.text)
+            location= 'cph'
+            if div_info[i].div.span is not None:
+                location = div_info[i].div.span.text.replace(',',' ')
+            #print(location)
+            #print(h3_title[i].a["href"])            
+            #print(div_time[i]["datetime"])
+            #print()
             link=h3_title[i].a["href"]
-            title=h3_title[i].text
-            company=div_info[i].text
-            time=div_time[i]["datetime"]
-            location="cph"
-            if "month" not in time and super().ex_filter(title, ItJobBankJob.EXECLUDE_TITLE) and super().ex_filter(company, ItJobBankJob.EXECLUDE_COMPANY):
+            title=h3_title[i].a.text
+            company=div_company[i].text
+            now = datetime.now() # current date and time
+            time = now.strftime("%Y-%m-%d")            
+            #if "month" not in time and super().ex_filter(title, ItJobBankJob.EXECLUDE_TITLE) and super().ex_filter(company, ItJobBankJob.EXECLUDE_COMPANY):
+            if super().ex_filter(title, ItJobBankJob.EXECLUDE_TITLE) and super().ex_filter(company, ItJobBankJob.EXECLUDE_COMPANY):
                 text = "{},{},{},{},##{}".format(title, company, location, time, link);
                 print(text)
                 result.append(text)
--- cs109/webScraping/job/StackOverflow.py
+++ cs109/webScraping/job/StackOverflow.py
@@ -12,22 +12,31 @@ class StackOverflowJob(Job):
 
     def parse(self, page_content):
         result = []
-        div = page_content.find_all("div", "-job-summary")
-        print("div size:" + str(len(div)))
-        divSize = len(div)    
-        for i in range(0, divSize):
-            link = div[i].div.h2.a["href"]
-            title = div[i].div.h2.a.text.replace("\n", "").strip().replace(",", "_")
+        #<h2 class="mb4 fc-black-800 fs-body3">
+        title_h2 = page_content.find_all("h2", "mb4 fc-black-800 fs-body3")
+        #<h3 class="fc-black-700 fs-body1 mb4">
+        company_h3=page_content.find_all("h3", "fc-black-700 fs-body1 mb4")
+        timeDiv = page_content.find_all("div", "mt4 fs-caption fc-black-500 grid gs4 gsx fw-wrap")
+        print("div size:" + str(len(title_h2)))
+        #divSize = len(div)
+        j=0
+        for i in range(0, len(title_h2)):
+            #if div[i].div is None or div[i].div.h2 is None or div[i].div.h2.a is None:
+            #    continue
+            link = "https://stackoverflow.com"+title_h2[i].a["href"]
+            title = title_h2[i].a.text.replace("\n", " ").strip().replace(",", "_")
             #print(title)
-            company = div[i].contents[3].span.text#.contents[1].text            
+            company = company_h3[i].span.text.replace("\n", " ").replace("\r", " ").strip()
             #company = div[i].children[1]#.contents[1].text
             #print("c1" + company)
-            company = company.replace(",", "_").replace("\n", "").replace("\r", "").strip()
+            company = company.replace(",", "_").replace("\n", " ").replace("\r", " ").strip()            
             #print(company)
-            time = div[i].div.contents[5].text
-            #print(time)
-            location = div[i].contents[3].contents[3].text.replace("\n", "").replace("\r", "").replace("-", "").strip().replace(",", "_")
-            #print(location)
+            time = timeDiv[j].div.text.replace(",", "_").replace("\n", " ").replace("\r", " ").strip()
+            j = j+1
+            #print("time -> " + time)
+            location = company_h3[i].contents[3].text.replace(",", "_").replace("\n", " ").replace("\r", " ").strip()
+            #.contents[1].text            #div[i].contents[3].contents[3].text.replace("\n", "").replace("\r", "").replace("-", "").strip().replace(",", "_")
+            #print("location -> " + location)
             if "month" not in time and super().ex_filter(title, StackOverflowJob.EXECLUDE_TITLE) and super().ex_filter(company, StackOverflowJob.EXECLUDE_COMPANY):
                 text = "{},{},{},{},##{}".format(title, company, location, time, link);
                 #print(text)
