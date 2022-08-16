a589eb887dd02a61bd94b09958253e130210a0d6
sujoe <sujoe77@gmail.com>
2022-08-16 21:02:22 +0200

    update Vision

--- Vision/Red Object Detection/test1/test1.cpp
+++ Vision/Red Object Detection/test1/test1.cpp
@@ -3,78 +3,132 @@
 #include <iostream>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
 
 using namespace cv;
 using namespace std;
 
- int main( int argc, char** argv )
- {
-    VideoCapture cap(0); //capture the video from web cam
+static void on_trackbar(int, void*)
+{
+	/*alpha = (double)alpha_slider / alpha_slider_max;
+	beta = (1.0 - alpha);
+	addWeighted(src1, alpha, src2, beta, 0.0, dst);
+	imshow("Linear Blend", dst);*/
+}
 
-    if ( !cap.isOpened() )  // if not success, exit program
-    {
-         cout << "Cannot open the web cam" << endl;
-         return -1;
-    }
+static int doTrack(int argc, char** argv){
+	VideoCapture cap(0); //capture the video from web cam
 
-    namedWindow("Control",WINDOW_AUTOSIZE); //create a window called "Control"
+	if (!cap.isOpened())  // if not success, exit program
+	{
+		cout << "Cannot open the web cam" << endl;
+		return -1;
+	}
 
-	int iLowH = 0;
+	namedWindow("Control", WINDOW_AUTOSIZE); //create a window called "Control"
+
+	int iLowH = 100;
 	int iHighH = 179;
 
-	int iLowS = 0; 
+	int iLowS = 0;
 	int iHighS = 255;
 
 	int iLowV = 0;
 	int iHighV = 255;
 
 	//Create trackbars in "Control" window
-	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-	createTrackbar("HighH", "Control", &iHighH, 179);
+	createTrackbar("LowH", "Control", &iLowH, 179, on_trackbar); //Hue (0 - 179)
+	createTrackbar("HighH", "Control", &iHighH, 179, on_trackbar);
 
-	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-	createTrackbar("HighS", "Control", &iHighS, 255);
+	createTrackbar("LowS", "Control", &iLowS, 255, on_trackbar); //Saturation (0 - 255)
+	createTrackbar("HighS", "Control", &iHighS, 255, on_trackbar);
 
-	createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
-	createTrackbar("HighV", "Control", &iHighV, 255);
+	createTrackbar("LowV", "Control", &iLowV, 255, on_trackbar);//Value (0 - 255)
+	createTrackbar("HighV", "Control", &iHighV, 255, on_trackbar);
 
-    while (true)
-    {
-        Mat imgOriginal;
+	while (true)
+	{
+		Mat imgOriginal;
 
-        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 
-         if (!bSuccess) //if not success, break loop
-        {
-             cout << "Cannot read a frame from video stream" << endl;
-             break;
-        }
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
 
-		 Mat imgHSV;
+		Mat imgHSV;
 
 		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-	
+
 		Mat imgThresholded;
 
 		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-      
+
 		//morphological opening (removes small objects from the foreground)
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-		dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
 		//morphological closing (removes small holes from the foreground)
-		dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
 		imshow("Thresholded Image", imgThresholded); //show the thresholded image
 		imshow("Original", imgOriginal); //show the original image
 
-        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-       {
-            cout << "esc key is pressed by user" << endl;
-            break; 
-       }
-    }
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
 
-   return 0;
+	return 0;
 }
+
+static int example(int argc, char** argv) {
+	if (argc != 2)
+	{
+		cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
+		return -1;
+	}
+	Mat image;
+	image = imread(argv[1], IMREAD_COLOR); // Read the file
+	if (image.empty()) // Check for invalid input
+	{
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+	imshow("Display window", image); // Show our image inside it.
+	waitKey(0); // Wait for a keystroke in the window
+	return 0;
+}
+
+
+int main( int argc, char** argv )
+{
+	/*if (argc != 2)
+	{
+		cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
+		return -1;
+	}*/	
+	//cout << argv[1] << endl;
+	Mat image = cvLoadImage("c:/Temp/1.jpg", IMREAD_COLOR); // Read the file
+	if (image.empty()) // Check for invalid input
+	{
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+	imshow("Display window", image); // Show our image inside it.
+	waitKey(0); // Wait for a keystroke in the window
+	return 0;
+}
+
+
