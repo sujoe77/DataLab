dfe1a2254bcd44597e3c444fef351fc6aab8a698
Zhou Su <sujoe77@gmail.com>
2026-04-05 08:32:14 +0200

    some cleanup

---
+++ cs109/webScraping/job/old.py
@@ -0,0 +1,49 @@
+EXECLUDE_TITLE = [
+    "frontend",
+    "c#",
+    "udvikler",
+    "android",
+    "analyst",
+    "devops",
+    "security",
+    "sale",
+    "microsoft",
+]
+EXECLUDE_COMPANY = ["dfds", "prodata", "Systematic", "test"]
+# KEYWORD_LIST=["Java","Data", "developer"]
+KEYWORD_LIST = ["Java", "Backend", "data", "architect"]
+STACKOVERFLOW_KEYWORD_LIST = ["Backend"]
+SLEEP = 6
+PAGE_SIZE = 5
+
+print("KEYWORD_LIST size: " + str(KEYWORD_LIST))
+print("EXECLUDE_COMPANY size: " + str(EXECLUDE_COMPANY))
+print("SLEEP: " + str(SLEEP))
+print("PAGE_SIZE: " + str(PAGE_SIZE))
+
+KEYWORD_LIST = ["Java", "Python"]
+KEYWORD_LIST = ["Architect", "IT Developer", "Software Engineer", "Backend Developer"]
+KEYWORD_LIST = ["Software Developer"]
+KEYWORD_LIST = ["Java"]
+
+KEYWORD_LIST_ARCHITECT = ["Architect"]
+KEYWORD_LIST = ["Java"]
+KEYWORD_LIST = ["Software Engineer"]
+KEYWORD_LIST = ["Backend Developer"]
+
+KEYWORD_LIST = ["Java Developer"]
+KEYWORD_LIST_PYTHON = ["Python"]
+KEYWORD_LIST = ["AI Developer", "AI Engineer"]
+KEYWORD_LIST_RUST = ["Rust"]
+KEYWORD_LIST = ["Java"]
+
+KEYWORD_LIST_ALL = ["Java", "Python",  "AI", "Rust", "Golang", "Scala", "C#"
+                "Software Engineer", "Developer", "Backend", "Architect", "Tech lead", "Backend", "Programmer",
+                "Fianance IT", "Fintech", "Banking IT", "Trading IT", "Bank IT"
+                ] #full list
+KEYWORD_LIST = ["Scala"]
+KEYWORD_LIST = ["C#"]
+
+KEYWORD_LIST = ["Software Developer"]
+KEYWORD_LIST = ["Fintech"]
+KEYWORD_LIST = ["Banking IT"]
